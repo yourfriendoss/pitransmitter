43d10ca4cb9076a4fdbb2d993ab025c91b6b19fe
yourfriend <friend@yourfriend.lv>
2022-05-10 18:53:37 +0300

    YASS

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ freq=$(jq .freq config.json)
 ps=$(jq .ps config.json)
 rt=$(jq .rt config.json)
 pty=$(jq .pty config.json)
+audio=$(jq .audio config.json)
 
 echo $freq $ps $rt $pty
 
 cd src
 make
 
-# assuming src/pi_fm_adv
+cd ..
 
-./src/pi_fm_adv --freq $freq --ps $ps --rt $rt --pty $pty
+./src/pi_fm_adv --freq $freq --ps $ps --rt $rt --pty $pty --audio $audio
